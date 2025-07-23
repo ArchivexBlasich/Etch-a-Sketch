72a42aa94079c81d9c0bb2f51ac4944380c88d31
ArchivexBlasich <fblasich0@gmail.com>
2025-07-23 11:28:05 -0300

    Add Load page event

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
-/*--------------------    Functions    ----------------------- */
-function getContentWidth (element) {
-  var styles = getComputedStyle(element)
+/*--------------------  Global Variables  ----------------------- */
+let NUMBER_OF_SQUARES = 16;
 
-  return element.clientWidth
-    - parseFloat(styles.paddingLeft)
-    - parseFloat(styles.paddingRight)
+/*--------------------    Functions    ----------------------- */
+function getContentWidth(element) {
+    var styles = getComputedStyle(element)
+
+    return element.clientWidth
+        - parseFloat(styles.paddingLeft)
+        - parseFloat(styles.paddingRight)
+}
+
+function addSquares(numSquares) {
+    for (let row = 0; row < numSquares; row++) {
+        for (let column = 0; column < numSquares; column++) {
+            let square = document.createElement("div");
+            mainContainer.appendChild(square);
+        }
+    }
 }
 
 /*-------------------- Events Handlers ----------------------- */
+function renderSquares(e) {
+    addSquares(NUMBER_OF_SQUARES);
 
+    const squares = document.querySelectorAll(".main-container div");
+
+    let containerSize = getContentWidth(mainContainer);
+    let squareSize = containerSize / 16;
+
+    squares.forEach(square => {
+        square.classList.add("square");
+        square.setAttribute("style", `height: ${squareSize}px; width: ${squareSize}px;`);
+    });
+}
 
 /*--------------------      Main      ----------------------- */
 const mainContainer = document.querySelector(".main-container");
 
-// Add all squares to the grid
-for (let row = 0; row < 16; row++) {
-    for (let column = 0; column < 16; column++) {
-        let square = document.createElement("div");
-        mainContainer.appendChild(square);
-    }
-}
+addEventListener("load", renderSquares);
 
-const squares = document.querySelectorAll(".main-container div");
-
-let containerSize = getContentWidth(mainContainer);
-let squareSize = containerSize / 16;
-
-squares.forEach(square => {
-    square.classList.add("square");
-    square.setAttribute("style", `height: ${squareSize}px; width: ${squareSize}px;`);
-});
