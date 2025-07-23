14dab712a6497afbb3d8cf9b37252890cf83c8a8
ArchivexBlasich <fblasich0@gmail.com>
2025-07-23 12:36:38 -0300

    Add functionality to eneable setnumber of squares per side for the new grid

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ function getContentWidth(element) {
         - parseFloat(styles.paddingRight)
 }
 
+function removeSquares() {
+    const squares = document.querySelectorAll(".main-container div");
+    squares.forEach(square => square.remove());
+}
+
 function addSquares(numSquares) {
     for (let row = 0; row < numSquares; row++) {
         for (let column = 0; column < numSquares; column++) {
@@ -25,13 +30,14 @@ function randomColor() {
 }
 
 /*-------------------- Events Handlers ----------------------- */
-function renderSquares(e) {
+function renderSquares() {
+    removeSquares();
     addSquares(NUMBER_OF_SQUARES);
 
     const squares = document.querySelectorAll(".main-container div");
 
     let containerSize = getContentWidth(mainContainer);
-    let squareSize = containerSize / 16;
+    let squareSize = containerSize / NUMBER_OF_SQUARES;
 
     squares.forEach(square => {
         square.classList.add("square");
@@ -40,16 +46,33 @@ function renderSquares(e) {
 }
 
 function hoverEffect(e) {
-    let squareBackground = e.target.style.background;
-    if (squareBackground === "") {
-        e.target.style.background = `rgb(${randomColor()} ${randomColor()} ${randomColor()} / ${HOVER_DARK}%)`;
-        if (HOVER_DARK !== 100)  HOVER_DARK += 10; 
+    e.target.style.background = `rgb(${randomColor()} ${randomColor()} ${randomColor()} / ${HOVER_DARK}%)`;
+    if (HOVER_DARK !== 100) HOVER_DARK += 10;
+}
+
+function changeGrid(e) {
+    let numSquares = Number(prompt("Numbers of squares: ", 16));
+    if (isNaN(numSquares)) return;
+
+    if (numSquares < 1 || numSquares > 100) {
+        if (numSquares < 1) alert("Select a number of square greater than 0");
+        if (numSquares > 100) alert("Select a number of square less than 100");
+        return;
     }
-    
+
+    NUMBER_OF_SQUARES = numSquares;
+    renderSquares();
 }
 
 /*--------------------      Main      ----------------------- */
 const mainContainer = document.querySelector(".main-container");
+const btnChangeGrid = document.querySelector(".change-grid");
 
+// Page Load Event Listener
 addEventListener("load", renderSquares);
+
+// Hover Effect Event Listener
 mainContainer.addEventListener("mouseover", hoverEffect);
+
+// Change Grid Event Listener
+btnChangeGrid.addEventListener("click", changeGrid);
